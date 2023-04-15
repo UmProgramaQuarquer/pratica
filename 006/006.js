f3e618d4e98eed300d3ae25f710ba60ff9a4f855
UmProgramaQuarquer <120744222+UmProgramaQuarquer@users.noreply.github.com>
2023-04-15 07:17:36 -0300

    Update 006.js

--- 006/006.js
+++ 006/006.js
@@ -1,15 +1,6 @@
-let num = 3
-
-function parImpar(n) {
-    if (n % 2 == 0) {
-        var valor = 'par'
-    } else if (n % 2 != 0){
-        var valor = 'ímpar'
-    }
-
-    return valor
+var valor = 10;
+while (valor >= 0) {
+    
 }
 
-var arr = [0, 1, 2]
-
-console.log(`O número ${arr.length}.`)
+console.log(`Incremento: ${incremento} | Decremento: ${decremento}`)
